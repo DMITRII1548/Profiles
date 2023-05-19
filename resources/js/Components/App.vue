18231d4a014a9d257f85921c76f4746ef151df06
DMITRII1548 <korsunov2006@list.ru>
2023-05-19 20:19:54 +0600

    Tailwind and vue3js imported

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import '../css/app.css'
+import { createApp, VueElement } from "vue/dist/vue.esm-bundler";
+import AppComponent from './components/App.vue';
+
+const app = createApp({
+    components: {
+        AppComponent,
+    }
+});
+
+app.mount('#app');
